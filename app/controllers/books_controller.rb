e05b80b05250437487ba8ba9fd781ecef840c238
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-09 09:52:58 +0000

    [update]if else redirect追加　rb app contorller books_conntroller記載

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    if book.save
+       redirect_to book_path(book.id), notice: 'Book was successfully created.' 
+    else
+      redirect_to books_path
+    end  
   end
 
   def show
@@ -25,8 +28,9 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update (book_params)
-    redirect_to book_path(book)
+     if book.update (book_params)
+        redirect_to book_path(book), notice: 'Book was successfully updated.'
+     end
   end
   
   def destroy
