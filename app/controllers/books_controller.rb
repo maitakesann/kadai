ad348353836c227cc1447deefd52cc7e1a5bbccd
maitakesann <kankore1204a@yahoo.co.jp>
2021-04-10 02:35:28 +0000

    [Add]edit update にrender追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,9 +33,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-     if book.update (book_params)
+    @book = Book.find(params[:id])
+      if @book.update (book_params)
         redirect_to book_path(book), notice: 'Book was successfully updated.'
+      else
+        render :edit
+      
       end
   end
   
